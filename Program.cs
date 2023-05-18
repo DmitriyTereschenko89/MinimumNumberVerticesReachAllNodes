6d6e287b41d7b76b29d6761802e99c9f57f8d26f
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-05-18 12:26:47 +0300

    update solution

--- Program.cs
+++ Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public class MinimumNumberVerticesReachAllNodes
+        {
+            public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+            {
+                bool[] inComingNode = new bool[n];
+                foreach (var edge in edges)
+                {
+                    inComingNode[edge[1]] = true;
+                }
+                List<int> smallestSetOfVertices = new();
+                for (int node = 0; node < n; ++node)
+                {
+                    if (!inComingNode[node])
+                    {
+                        smallestSetOfVertices.Add(node);
+                    }
+                }
+                return smallestSetOfVertices.ToArray();
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MinimumNumberVerticesReachAllNodes minimumNumberVerticesReachAllNodes = new();
+            foreach(int n in minimumNumberVerticesReachAllNodes.FindSmallestSetOfVertices(6,
+                new int[][] { new int[] { 0, 1 }, new int[] { 0, 2 }, new int[] { 2, 5 }, new int[] { 3, 4 }, new int[] { 4, 2 } }))
+            {
+                Console.Write(n);
+            }
+            Console.WriteLine();
+            foreach (int n in minimumNumberVerticesReachAllNodes.FindSmallestSetOfVertices(5,
+                new int[][] { new int[] { 0, 1 }, new int[] { 2, 1 }, new int[] { 3, 1 }, new int[] { 1, 4 }, new int[] { 2, 4 } }))
+            {
+                Console.Write(n);
+            }
+            Console.WriteLine();
         }
     }
 }
